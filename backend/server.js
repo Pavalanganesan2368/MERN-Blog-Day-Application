d3fe57b46fe97a7a6d50636fcc4fcf83c9a37f9f
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2026-07-20 12:30:20 +0530

    Modification of server.js file

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/posts", postRouter);
 app.use("/api/auth", userRouter);
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 })
 
